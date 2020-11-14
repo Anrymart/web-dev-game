53f220bec4eba9dfed086688916a61d0a7f07b00
Anrymart <a.mart@list.ru>
2020-11-14 11:29:11 +0400

    auth

---
+++ backend/src/main/kotlin/com/kotlinspringvue/backend/controller/BackendController.kt
@@ -0,0 +1,42 @@
+package com.kotlinspringvue.backend.controller
+
+import com.kotlinspringvue.backend.jpa.User
+import com.kotlinspringvue.backend.model.Greeting
+import com.kotlinspringvue.backend.repository.PersonRepository
+import com.kotlinspringvue.backend.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = ["*"], maxAge = 3600)
+class BackendController {
+
+    @Autowired
+    lateinit var personRepository: PersonRepository
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @GetMapping("/persons")
+    fun getPersons() = personRepository.findAll()
+
+    @GetMapping("/usercontent")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @ResponseBody
+    fun getUserContent(authentication: Authentication): String {
+        val user: User = userRepository.findByUsername(authentication.name).get()
+        return "Hello " + user.firstName + " " + user.lastName + "!"
+    }
+
+    @GetMapping("/admincontent")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseBody
+    fun getAdminContent(): String {
+        return "Admin's content"
+    }
+
+}
